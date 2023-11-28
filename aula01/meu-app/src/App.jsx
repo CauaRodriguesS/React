0b0dfa0a4f7af96e93728a0bb4bb04b62fe4d027
CauaRodriguesS <caua20005@gmail.com>
2023-11-28 00:49:16 -0300

    fazendo css

--- aula01/meu-app/src/App.jsx
+++ aula01/meu-app/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Frase from './components/Frase';
 import Pessoa from './components/Pessoa';
 import SayMyName from './components/SayMyName'
 function App() {
@@ -9,10 +10,8 @@ function App() {
   return (
     <div  className="App" >
       <h1>testando css</h1>
-     <SayMyName nome='mateus'/>
-     <SayMyName nome='Cauã'/>
-     <SayMyName nome = {nome }/>
-     <Pessoa nome="caua" idade = '17' profissao = 'programador' foto = 'https://via.placeholder.com/150'/>
+      <Frase/>
+    
     </div>
   );
 }
--- aula01/meu-app/src/components/Frase.jsx
+++ aula01/meu-app/src/components/Frase.jsx
@@ -1,7 +1,8 @@
+import styles from './Frase.module.css'
 function Frase(){
     return(
-        <div>
-            <p>este é um componente com uma frase</p>
+        <div className={styles.Conteiner}>
+            <p className={styles.content}>este é um componente com uma frase</p>
         </div>
     )
 }
